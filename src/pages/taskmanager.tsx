2a117695b3f88d913a0ac7e70a3bb4cdfdb999cb
NoelVegaJr <noeljr126@gmail.com>
2023-01-30 03:46:07 -0500

    initial taskmanager page with filters

---
+++ src/pages/taskmanager.tsx
@@ -0,0 +1,136 @@
+import { useEffect, useState } from "react";
+import { XMarkIcon } from '@heroicons/react/24/solid'
+import { ISelectOption } from "@/types/types";
+import { ProjectsManager } from "@/classes/classes";
+import Select from "@/components/Select";
+import ProjectTaskListTable from "@/components/ProjectTaskListTable";
+import MemberProjectListTable from "@/components/MemberProjectListTable";
+
+const projects = [
+  {
+    id: '1',
+    title: 'Websocket Updates',
+    tasks: [
+      {
+        id: '1',
+        title: 'Add to Socket.IO',
+        assignedTo: 'Stuart',
+        estimatedHours: 2
+      },
+      {
+        id: '2',
+        title: 'Enable Broadcasting',
+        assignedTo: 'Stuart',
+        estimatedHours: 5
+      },
+      {
+        id: '3',
+        title: 'Adjust Interface',
+        assignedTo: 'Stuart',
+        estimatedHours: 3
+      }
+    ],
+  },
+  {
+    id: '2',
+    title: 'Angular Upgrade',
+    tasks: [
+      {
+        id: '4',
+        title: 'Upgrade Angular',
+        assignedTo: 'Lan',
+        estimatedHours: 15
+      },
+      {
+        id: '5',
+        title: 'Fix Broken Things',
+        assignedTo: 'Stuart',
+        estimatedHours: 10
+      },
+      {
+        id: '6',
+        title: 'Test',
+        assignedTo: 'Lan',
+        estimatedHours: 10
+      }
+    ]
+  },
+  {
+    id: '3',
+    title: 'E-Commerce Website',
+    tasks: [
+      {
+        id: '7',
+        title: 'Product Pages',
+        assignedTo: 'Adam',
+        estimatedHours: 5
+      },
+      {
+        id: '8',
+        title: 'Shopping Cart',
+        assignedTo: 'Tyler',
+        estimatedHours: 10
+      },
+      {
+        id: '9',
+        title: 'My Account',
+        assignedTo: 'Adam',
+        estimatedHours: 5
+      }
+    ]
+  }
+]
+
+const categoryOptions: ISelectOption[] = [{ label: 'Projects', id: '1' }, { label: 'Members', id: '2' }];
+const baseFilterOptions: ISelectOption[] = [{ label: 'All', id: 'All' }]
+
+export default function TaskManagerPage() {
+  const manager = new ProjectsManager(projects);
+  const [category, setCategory] = useState(categoryOptions[0]);
+  const [filterOptions, setFilterOptions] = useState(
+      baseFilterOptions.concat(manager.projects.map(p => ({ label: p.title, id: p.id })))
+    )
+    
+  const [filter, setFilter] = useState(filterOptions[0]);
+  const [showDrawer, setShowDrawer] = useState(false);
+
+  useEffect(() => {
+    let options = baseFilterOptions;
+    if (category.label === 'Projects') {
+      options = options.concat(manager.projects.map(p => {
+        return { label: p.title, id: p.id }
+      }))
+    } else {
+      options = options.concat(manager.members.map(m => {
+        return { label: m.name, id: m.id }
+      }))
+    }
+    setFilterOptions(options);
+    setFilter(options[0]);
+  }, [category])
+
+  const handleCategoryChange = (option: ISelectOption) => {
+    setCategory(option);
+  }
+  const handleFilterChange = (option: ISelectOption) => {
+    setFilter(option)
+  }
+
+  return (
+    <div className="h-screen w-full flex overflow-hidden" onClick={() => setShowDrawer(false)}>
+      <div className="max-w-4xl w-full mx-auto">
+        <div className="flex gap-4 mb-6">
+          <Select title="Category" selected={category} options={categoryOptions} onChange={handleCategoryChange} />
+          <Select title="Filter" selected={filter} options={filterOptions} onChange={handleFilterChange} />
+        </div>
+        {category.label === 'Projects' && <ProjectTaskListTable onClick={() => setShowDrawer(true)} filter={filter} manager={manager} />}
+        {category.label === 'Members' && <MemberProjectListTable onClick={() => setShowDrawer(true)} filter={filter} manager={manager} />}
+      </div>
+      <div className={`h-full border ${showDrawer ? 'w-1/4' : 'w-0'} transition-all duration-300`}>
+        <div className="flex justify-end p-4">
+          <XMarkIcon className="text-red-600 w-6 h-6" onClick={() => setShowDrawer(false)} />
+        </div>
+      </div>
+    </div>
+  )
+}
