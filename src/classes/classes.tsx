9b885aea8334812277e8a10e91e13a7558c43193
NoelVegaJr <noeljr126@gmail.com>
2023-01-30 03:41:30 -0500

    initial project and project-manager classes

---
+++ src/classes/classes.tsx
@@ -0,0 +1,85 @@
+import type {ITask, IProject, IMember} from '@/types/types';
+
+export class Project {
+  private _id: string;
+  private _title: string;
+  private _tasks: Omit<ITask, 'projectTitle'>[]
+
+  constructor(project: IProject) {
+    this._id = project.id;
+    this._title = project.title;
+    this._tasks = project.tasks;
+  }
+
+  get id() {
+    return this._id;
+  }
+
+  get title() {
+    return this._title;
+  }
+
+  get tasks() {
+    return this._tasks;
+  }
+
+  get members() {
+    const memberIdSet = new Set();
+    const members: Omit<IMember, 'projects'>[] = [];
+
+    this.tasks.forEach(task => {
+      if (!memberIdSet.has(task.assignedTo)) {
+        memberIdSet.add(task.assignedTo);
+        members.push({ id: task.assignedTo, name: task.assignedTo });
+      }
+    })
+    return members;
+  }
+
+  get totalHours() {
+    return this.tasks.reduce((hours, task) => hours += task.estimatedHours, 0);
+  }
+}
+
+
+
+
+export class ProjectsManager {
+  private _projects: Map<string, Project> = new Map();
+  private _members: Map<string, IMember> = new Map();
+
+  constructor(projects: Project[] = []) {
+    projects.forEach(project => this.add(new Project(project)))
+  }
+
+  add(project: Project) {
+    this._projects.set(project.id, project);
+    project.members?.map(pm => {
+      if (!pm) return;
+      const existingMember = this._members.get(pm.id);
+
+      if (!existingMember) {
+        this._members.set(pm.id, { id: pm.id, name: pm.name, projects: [project] })
+      } else {
+        const updatedMember = { ...existingMember, projects: [...existingMember.projects, project] }
+        this._members.set(existingMember.id, updatedMember);
+      }
+    })
+  }
+
+  get projects() {
+    return Array.from(this._projects.values());
+  }
+
+  getProject(id: string) {
+    return this._projects.get(id);
+  }
+
+  get members() {
+    return Array.from(this._members.values());
+  }
+
+  getMember(id: string) {
+    return this._members.get(id);
+  }
+}
