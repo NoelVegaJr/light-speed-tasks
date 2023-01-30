7472cc60fc4745d0847039c6709a46576ecfc2a0
NoelVegaJr <noeljr126@gmail.com>
2023-01-30 03:42:23 -0500

    initial type for Projects, Select, Members and Tasks

---
+++ src/types/types.ts
@@ -0,0 +1,33 @@
+import { Project, ProjectsManager } from "@/classes/classes";
+
+
+export interface IMember {
+  id: string;
+  name: string;
+  projects: Project[];
+}
+
+export interface IProject {
+  id: string;
+  title: string;
+  tasks: Omit<ITask, 'projectTitle'>[]
+}
+
+export interface ITask {
+  id: string;
+  title: string;
+  assignedTo: string;
+  estimatedHours: number;
+  projectTitle: string;
+}
+
+export interface ISelectOption {
+  label: string;
+  id: string;
+}
+
+export interface ITableProps {
+  filter: ISelectOption;
+  manager: ProjectsManager;
+  onClick: () => void;
+}
